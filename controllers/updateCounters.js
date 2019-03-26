0c2abdfc51bbe0532ba5498d31eb598a6e6afc8b
aa-nikitin <a.nikitin91@mail.ru>
2019-03-26 11:26:50 +0300

    add updateCounters

--- controllers/updateCounters.js
+++ controllers/updateCounters.js
@@ -1,10 +1,17 @@
 const Counters = require('../models/counters');
 
 module.exports = (req, res, next) => {
-  const { name, count } = req.body;
-  Counters.updateOne({ name: name }, { $set: { count: count } }, { new: true })
-    .then(counters => {
-      return res.status(200).json(counters);
-    })
-    .catch(next);
+  const jsonCounts = req.body;
+  // console.log(jsonCounts.arrCounts[1]);
+  const { arrCounts } = jsonCounts;
+  // const { arrCounts } = JSON.parse(jsonCounts);
+  arrCounts.forEach(item => {
+    const { name, count } = item;
+    Counters.updateOne(
+      { name: name },
+      { $set: { count: count } },
+      { new: true }
+    ).catch(next);
+  });
+  return res.status(200).json({ maessage: 'Счетчики успешно изменены' });
 };
